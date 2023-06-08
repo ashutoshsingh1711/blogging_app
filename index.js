ac06a1f5648c4754c6ad29eb16258b7116edf0b8
Ashutosh Singh <ashutosh.singh32@infosys.com>
2023-06-08 20:17:40 +0530

    added express.json to parse incoming data

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
+//This middleware parses the incoming request's body if it is in JSON format and makes it available as req.body in the subsequent middleware or route handlers.
+// By using express.json(), you can easily handle JSON data in your Express application. It automatically parses the request body and converts it into a JavaScript object
+app.use(express.json()); 
+
 const connectWithDb = require('./config/database');
 connectWithDb();
 
